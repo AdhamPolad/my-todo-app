8d76b2bb7c53e7b37b566f530c797bb8107fd7c7
Poaldzade <edhempoladzade2@gmail.com>
2024-05-24 15:39:13 +0400

    initial commit

---
+++ functions.py
@@ -0,0 +1,17 @@
+
+FilePath = 'text.txt'
+
+def get_todos(filepath = FilePath):
+    with open(filepath,'r') as file_local:
+        todos_local = file_local.readlines()
+
+    return todos_local
+
+
+def write_todos(todos_local,filepath = FilePath):
+    with open(filepath, 'w') as file_local:
+        file_local.writelines(todos_local)
+
+
+def count(phrase):
+    return phrase.count(".")
